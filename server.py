004ffbfb0b6af7858b5ee790e10d0617af1ef06d
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2023-07-04 19:00:25 -0400

    server and client are able to connect

--- server.py
+++ server.py
@@ -7,16 +7,25 @@ port=8080
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # establish TCP connection with server
 mySocket.bind((host, port))     # bind host and port
 
-# using dictionary to store clients
+# using dictionary to store clients information
 connectedClients={}
 clientAddresses={}
 
+def handle_clients(client_connection,client_address):
+    name = client_connection.recv(1024).decode()
+    greeting = "Welcome, " + name + ". You can type 'quit' to leave chatroom."
+    client_connection.recv(bytes(greeting, "utf8"))
+    message = name + "has joined the chatroom!"
+    broadcast(bytes(message, "utf8"))
+    
 def accept_connection():
     while True:
         client_connection, client_address = mySocket.accept()
         print(client_address, " has connected!")
         client_connection.send("Welcome to iChat. Please enter your name".encode('utf8'))
         clientAddresses[client_connection]=client_address   # store client object and address in dictionary 
+        
+        Thread(target=handle_clients, args=(client_connection,client_address)).start    # invoke function using thread
 
 if __name__=="__main__":
     mySocket.listen(10)
