16a2ccd62e894c761f03013ed198ca361a724835
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2023-07-04 18:55:19 -0400

    Implemented threads to accept multiple client connections.

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 host='localhost'
 port=8080
@@ -6,11 +7,22 @@ port=8080
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # establish TCP connection with server
 mySocket.bind((host, port))     # bind host and port
 
-mySocket.listen() # keep listening for connections
-print("server is listening...")
-connection, address = mySocket.accept()
+# using dictionary to store clients
+connectedClients={}
+clientAddresses={}
 
-message = "connection successful..."
-connection.send(message.encode())    # send encoded message to connection
+def accept_connection():
+    while True:
+        client_connection, client_address = mySocket.accept()
+        print(client_address, " has connected!")
+        client_connection.send("Welcome to iChat. Please enter your name".encode('utf8'))
+        clientAddresses[client_connection]=client_address   # store client object and address in dictionary 
 
-connection.close()
+if __name__=="__main__":
+    mySocket.listen(10)
+    print("server listening to client reqs")
+    
+    # by using threads, the server can handle multiple clients
+    serverThread = Thread(target=accept_connection)
+    serverThread.start()
+    serverThread.join() # join function lets multiple executions to take place at once
